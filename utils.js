92aea2b7c66068649fbc619c1b4bcbde3c7f6099
root <alexgue404@gmail.com>
2020-01-15 11:30:20 +0000

    init

---
+++ utils.js
@@ -0,0 +1,24 @@
+module.exports.combineVariables = combineVariables;
+
+
+function auxiliar(mapCombine, currentMap, currentDeep, possibilities) {
+    var key = Object.keys(mapCombine)[currentDeep]
+    for (var val of mapCombine[key]) {
+        currentMap[key] = val
+        var internalDeep = currentDeep + 1;
+        if (internalDeep < Object.keys(mapCombine).length) {
+            auxiliar(mapCombine, currentMap, internalDeep, possibilities)
+        }
+        else {
+            possibilities.push(Object.assign({}, currentMap));
+        }
+    }
+
+}
+
+function combineVariables(mapCombine) {
+    var possibilities = [];
+    auxiliar(mapCombine, {}, 0, possibilities);
+    return possibilities;
+
+}
